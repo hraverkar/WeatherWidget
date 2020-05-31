38d599f37aaf75449025804150ac54bdea6d56bd
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-05-31 17:04:09 +0530

    Enviroment data added.
    Enviroment data added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  apiKey: 'a3f07567f71036c71a8e7a860b66a706'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // apiUrl: please enter URL
+  // apiKey: please enter api key
 };
 
 /*
